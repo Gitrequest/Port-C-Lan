0aac55a1244cada4a092ec8e3f2347c6850fd354
Xiras <benny.lueffe@gmail.com>
2021-10-20 16:50:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace C_Networking_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.iptext = new System.Windows.Forms.TextBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.consoleOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(387, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // iptext
+            // 
+            this.iptext.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.iptext.Location = new System.Drawing.Point(12, 27);
+            this.iptext.Name = "iptext";
+            this.iptext.Size = new System.Drawing.Size(369, 16);
+            this.iptext.TabIndex = 1;
+            this.iptext.KeyDown += new System.Windows.Forms.KeyEventHandler(this.iptext_KeyDown);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(478, 147);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(8, 8);
+            this.hScrollBar1.TabIndex = 2;
+            // 
+            // consoleOutput
+            // 
+            this.consoleOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.consoleOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.consoleOutput.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.consoleOutput.ForeColor = System.Drawing.Color.White;
+            this.consoleOutput.Location = new System.Drawing.Point(12, 68);
+            this.consoleOutput.Multiline = true;
+            this.consoleOutput.Name = "consoleOutput";
+            this.consoleOutput.ReadOnly = true;
+            this.consoleOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.consoleOutput.Size = new System.Drawing.Size(776, 370);
+            this.consoleOutput.TabIndex = 4;
+            this.consoleOutput.TextChanged += new System.EventHandler(this.consoleOutput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(645, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Powered by Energydrinks ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Enabled = false;
+            this.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_cancel.Location = new System.Drawing.Point(478, 20);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 28);
+            this.btn_cancel.TabIndex = 6;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.consoleOutput);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.iptext);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Port Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox iptext;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox consoleOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Linq;
+using System.Net;
+
+namespace C_Networking_Tool
+{
+    // Main Frame / Main Thread
+    public partial class Form1 : Form
+    {
+
+        CancellationTokenSource CancelToken = null; //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+
+            InitializeComponent();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //Validating IP-----------------------------------------------------------
+        public void IsValidIPAddress(string IpAddress)
+        {
+            try
+            {
+                IPAddress IP;
+                if (IpAddress.Count(c => c == '.') == 3)
+                {
+                    bool flag = IPAddress.TryParse(IpAddress, out IP);
+                    if (flag)
+                    {
+                        iptext.Clear();
+                        consoleOutput.Clear();
+                        consoleOutput.Text = "Starting....\n";
+                        
+                        //button control
+                        iptext.Enabled = false;
+                        button1.Enabled = false;
+                        btn_cancel.Enabled = true;
+
+                        //Creating new Thread ----------------------------------------------------
+                        Thread RunScanMethod = new Thread(new ParameterizedThreadStart(RunScan));
+                        RunScanMethod.Start(IP);
+                        //------------------------------------------------------------------------
+
+                    }
+                    else
+                    {
+                        
+                        iptext.Clear();
+                        consoleOutput.Clear();
+                        consoleOutput.Text = "You entered a not valid IP. Try again.";
+                    }
+                }
+                else
+                {
+                    iptext.Clear();
+                    consoleOutput.Clear();
+                    consoleOutput.Text = "You entered a not valid IP. Try again.";
+                }
+            }
+            catch (Exception) { }
+        }
+
+        //------------------------------------------------------------------------
+        //Ports
+        private static int[] Ports = new int[]
+        {
+            20, //FTP Data Transfer
+            21, //FTP Command Control
+            22, //FTPS/SSH
+            23, //Telnet
+            25, //SMTP
+            26, //SMTP
+            53, //DNS
+            80, //HTTP
+            110, //POP3
+            143, //IMAP
+            443, //HTTPs
+            587, //SMTP SSL
+            993, //IMAP SSL
+            995, //POP3 SSL
+            2077, // WebDAV/WEBDisk
+            2078, // Webdav/webdisk ssl
+            2082, // Cpanel
+            2083, // Cpanel ssl
+            2086, // WHM -webhost manager
+            2087, // whm - ssl
+            2095, //webmail
+            2096, //webmail ssl
+            3306, // mysql
+
+
+
+        };
+
+        //Port Scan Method / New Thread
+        private void RunScan(object IP)
+        {
+            CancelToken = new CancellationTokenSource();
+            var token = CancelToken.Token;
+
+            string IPX = IP.ToString();
+            string result = "Running...";
+            consoleOutput.Invoke(new MethodInvoker(delegate () { consoleOutput.Text = result; }));
+            foreach (int s in Ports)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    //token.ThrowIfCancellationRequested();
+                    result = null;
+                    return;
+                }
+                using (TcpClient Scan = new TcpClient())
+                {
+                    try 
+                    {
+                        Scan.Connect(IPX, s);
+                        result += Environment.NewLine + ($"[{s}]" + " | Open\n");
+                        consoleOutput.Invoke(new MethodInvoker(delegate () { consoleOutput.Text = result; }));
+
+                    }
+                    catch 
+                    {
+                        result += Environment.NewLine + ($"[{s}]" + " | Closed\n");
+                        consoleOutput.Invoke(new MethodInvoker(delegate () { consoleOutput.Text = result; }));
+                    }
+                }
+
+            }
+            result += Environment.NewLine + "Port Scan finished. \n";
+            consoleOutput.Invoke(new MethodInvoker(delegate () { consoleOutput.Text = result; }));
+            button1.Invoke(new MethodInvoker(delegate () { button1.Enabled = true; }));
+            iptext.Invoke(new MethodInvoker(delegate () { iptext.Enabled = true; }));
+            btn_cancel.Invoke(new MethodInvoker(delegate () { btn_cancel.Enabled = false; }));
+
+
+        }
+
+        // Scan Button Click
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(consoleOutput.Text))
+            {
+                consoleOutput.Text = "Please Enter IP first\n";
+            }
+            
+            
+            consoleOutput.Text = "Starting....\n";
+            string IP = iptext.Text;
+            IsValidIPAddress(IP);
+
+        }
+
+        private void consoleOutput_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Cancel Button Click
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            CancelToken.Cancel();
+            Thread.Sleep(3000);
+            iptext.Clear();
+            consoleOutput.Invoke(new MethodInvoker(delegate () { consoleOutput.Text = "Cancelled"; }));
+            btn_cancel.Enabled = false;
+            button1.Enabled = true;
+            iptext.Enabled = true;
+        }
+
+        // IP Text Enter
+        private void iptext_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                string IP = iptext.Text;
+                IsValidIPAddress(IP);
+
+            }
+        }
+    }
+}
+
+
+
+
